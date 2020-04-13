2aa6d0f6fbe5af941432fa489fe84c0b34502d85
ali yasin <ali99yasin@gmail.com>
2020-04-13 23:46:40 +0300

    commited

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { join } from 'path';
 import open from 'open';
+import webpack from 'webpack'
+import config from '../webpack.config.dev.js'
 const app = express()
+const compiler = webpack(config)
 
 const port = 5000;
-
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '../src/index.html'))
 })
